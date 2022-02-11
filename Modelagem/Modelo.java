24b55096195837e800a6b64130beb559b8fdf6f7
bolander2000 <gigio2005_brigati@hotmail.com>
2022-02-11 18:20:20 -0300

    Add files via upload

---
+++ Modelagem/Modelo.java
@@ -0,0 +1,23 @@
+package Modelagem;
+
+public class Modelo {
+
+	private String nome;
+        
+        public Modelo(String nome){
+            super();
+            this.nome = nome;
+        }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    @Override
+    public String toString(){
+        return nome;
+    }
+}    
